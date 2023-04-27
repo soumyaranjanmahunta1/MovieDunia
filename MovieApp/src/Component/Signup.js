be652a212b1fba3907e2518b3453bbe365a9adaf
soumyaranjanmahunta1 <happysoumya3@gmail.com>
2023-04-27 17:54:19 +0530

    signup issues

--- MovieApp/src/Component/Signup.js
+++ MovieApp/src/Component/Signup.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { TailSpin } from "react-loader-spinner";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { usersRef } from "../Firebase/Firebase";
+import bcrypt from "bcrypt"
+import {getAuth,RecaptchaVerifier,signInWithPhoneNumber } from "firebase/auth"
+import { Container } from "@mui/system";
+import swal from "sweetalert";
+import app from "../Firebase/Firebase"
+const auth = getAuth(app);
 export default function Signup() {
+    const navigate = useNavigate();
   const [form, setform] = useState({
     name: "",
     mobile: "",
@@ -9,7 +18,70 @@ export default function Signup() {
   });
   const [loading, setloading] = useState(false);
   const [otp, setotp] = useState("");
-  const [otpsend, setotpsend] = useState(true);
+  const [otpsend, setotpsend] = useState(false);
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+        },
+      },
+      auth
+    );
+  };
+  const requestOtp = () => {
+    setloading(true);
+    generateRecaptcha();
+    let appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, `+91${form.mobile}`, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        swal({
+          text: "OTP Sent",
+          icon: "success",
+          buttons: false,
+          timer: 3000,
+        });
+        setotpsend(true);
+        setloading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //  const verifyOTP = () => {
+  //    try {
+  //      setloading(true);
+  //      window.confirmationResult.confirm(otp).then((result) => {
+  //       //  uploadData();
+  //        swal({
+  //          text: "Sucessfully Registered",
+  //          icon: "success",
+  //          buttons: false,
+  //          timer: 3000,
+  //        });
+  //        navigate("/login");
+  //        setloading(false);
+  //      });
+  //    } catch (error) {
+  //      console.log(error);
+  //    }
+  // };
+    // const uploadData = async () => {
+    //   try {
+    //     const salt = bcrypt.genSaltSync(10);
+    //     var hash = bcrypt.hashSync(form.password, salt);
+    //     await addDoc(usersRef, {
+    //       name: form.name,
+    //       password: hash,
+    //       mobile: form.mobile,
+    //     });
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
   return (
     <div className="flex flex-col  mt-4  items-center ">
       {otpsend ? (
@@ -30,15 +102,22 @@ export default function Signup() {
                   name="number"
                   value={otp}
                   onChange={(e) => {
-                    setotp(e.target.otp)
+                    setotp(e.target.value);
                   }}
                   class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
             </div>
             <div class="p-2 w-full">
-              <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-black rounded text-lg">
-                {loading ? <TailSpin height={25} color="white" /> : "Confirm OTP"}
+              <button
+                onClick={verifyOTP}
+                class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-black rounded text-lg"
+              >
+                {loading ? (
+                  <TailSpin height={25} color="white" />
+                ) : (
+                  "Confirm OTP"
+                )}
               </button>
             </div>
           </div>
@@ -108,7 +187,10 @@ export default function Signup() {
               </div>
             </div>
             <div class="p-2 w-full">
-              <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-black rounded text-lg">
+              <button
+                onClick={requestOtp}
+                class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-black rounded text-lg"
+              >
                 {loading ? <TailSpin height={25} color="white" /> : "Sign up"}
               </button>
             </div>
@@ -123,6 +205,7 @@ export default function Signup() {
           </div>
         </>
       )}
+      <div id="recaptcha-container"></div>
     </div>
   );
 }
